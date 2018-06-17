1863eb4003b49dc9c9378f0943531f297483efe6
Richard Porter <gifted.primate@protonmail.com>
2018-06-17 09:17:09 -0400

    WIP: implements video hint dao, no methods implemented

---
+++ app/model/VideoHintDAO.scala
@@ -0,0 +1,24 @@
+package model
+
+import com.google.inject.Inject
+import model.models.VideoHint
+import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
+import org.bson.codecs.configuration.CodecRegistry
+import org.mongodb.scala.bson.codecs.{DEFAULT_CODEC_REGISTRY, Macros}
+import org.mongodb.scala.{MongoCollection, MongoDatabase}
+
+import scala.concurrent.ExecutionContext
+
+class VideoHintDAO @Inject()(mathbotDb: MongoDatabase)(implicit ec: ExecutionContext) {
+  final val collectionLabel = "video-hints"
+
+  val codecRegistry: CodecRegistry = fromRegistries(
+    fromProviders(
+      Macros.createCodecProvider[VideoHint]()
+    ),
+    DEFAULT_CODEC_REGISTRY
+  )
+
+  val collection: MongoCollection[VideoHint] =
+    mathbotDb.getCollection[VideoHint](collectionLabel).withCodecRegistry(codecRegistry)
+}
