1ca16551aa665e7c35466cd8aa8906dfb02647c7
Richard Porter <gifted.primate@protonmail.com>
2018-07-08 17:43:42 -0400

    bug fix for my last bug fix! adds correct evt check in commands component addToActiveFunc

--- ui/src/components/Commands.vue
+++ ui/src/components/Commands.vue
@@ -229,8 +229,8 @@ export default {
     },
     addToActiveFunc (evt) {
       const index = evt.item.getAttribute('data-function-index')
-      // console.log('INDEX IN ~ ', index);
-      if (evt.added) {
+      // console.log('INDEX IN ~ ', index)
+      if (evt.type === 'add') {
         api.activateFunction({tokenId: this.token.token_id, stagedIndex: index, activeIndex: evt.newIndex}, lambdas => {
           // console.log('NEW LAMBDAS ~ ', lambdas)
           this.$store.dispatch('updateLambdas', lambdas)
