0a3befb37dd41c39703c2d7e732a5dd7e38274dd
Richard Porter <gifted.primate@protonmail.com>
2018-07-15 17:40:32 -0400

    WIP: makes popover bucket pointer work with swipers

--- ui/src/services/PopoverBucket.js
+++ ui/src/services/PopoverBucket.js
@@ -4,13 +4,14 @@ class PopoverBucket {
     this.editingIndex = this.context.$store.getters.getEditingIndex
     this.$pointer = $('.pointer')
 
+    this.$methods = $('.methods')
+
     this.$selected = $(target)
     this.$selectedPosition = this.$selected.position()
     this.$selectedWidth = this.$selected.width()
     this._updatePointerPosition()
 
-    this.$functions = $('.functions')
-    this.startScrollTop = this.$functions.scrollTop()
+    this.selectedOffsetLeft = this.$selected.offset().left
 
     this.watcherRunning = true
     this._handlePointerVisibility = this._handlePointerVisibility.bind(this)
@@ -32,7 +33,7 @@ class PopoverBucket {
     if (this.$selected.attr('id') === 'open-staged') {
       left += this.$selectedWidth - pointerWidth * 1.35
     } else {
-      left += ((this.$selectedWidth / 2) - (pointerWidth / 4))
+      left += ((this.$selectedWidth / 2) - (pointerWidth / 4)) + this.$methods.width()
     }
 
     this.$pointer.css({left: left})
@@ -55,8 +56,7 @@ class PopoverBucket {
   }
 
   _handlePointerVisibility () {
-    const scrollTop = this.$functions.scrollTop()
-    this._togglePointer(scrollTop === this.startScrollTop)
+    this._togglePointer(this.$selected.offset().left === this.selectedOffsetLeft)
     if (this.watcherRunning) setTimeout(this._handlePointerVisibility, 100)
   }
 }
