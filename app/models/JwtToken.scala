98a908be3df9bfddabee3cd7d0ccb42585fa6d2d
Console Rastling <35277479+console-rastling@users.noreply.github.com>
2018-08-06 15:41:36 -0700

    Google oauth2 endpoint.

--- app/models/JwtToken.scala
+++ app/models/JwtToken.scala
@@ -1,15 +1,13 @@
 package models
 
-case class JwtToken(hd: String,
-                    iss: String,
+case class JwtToken(iss: String,
                     sub: String,
                     email: String,
                     email_verified: Boolean,
                     name: String,
                     picture: String,
                     given_name: String,
-                    family_name: String)
-{
+                    family_name: String) {
   def getIssuerShortName: String = {
     iss match {
       case "https://accounts.google.com" => "google-oauth2"
