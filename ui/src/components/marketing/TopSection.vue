71eaf0465609295b6b8ef92dc10e1112ea98a82a
Richard Porter <gifted.primate@protonmail.com>
2018-08-07 14:35:34 -0400

    media queries marketing pages up to ipad

--- ui/src/components/marketing/TopSection.vue
+++ ui/src/components/marketing/TopSection.vue
@@ -31,16 +31,6 @@
 import MathbotHeader from '../Mathbot_header'
 export default {
   name: 'TopSection',
-  computed: {
-    headerFont () {
-      if (this.windowWidth < 321) {
-        return 20
-      }
-    },
-    windowWidth () {
-      return window.innerWidth
-    }
-  },
   data () {
     return {
       imgResources: {
@@ -52,7 +42,8 @@ export default {
         planetFive: 'https://res.cloudinary.com/doohickey/image/upload/v1528476931/planet-5_va9bvg.png',
         'spaceShip': 'https://res.cloudinary.com/doohickey/image/upload/v1521989176/noun_1018446_cc_zvq5px.svg',
         'archedLine': 'https://res.cloudinary.com/doohickey/image/upload/v1521989179/Path_2_ggddw3.svg'
-      }
+      },
+      headerFont: 0
     }
   },
   components: {
@@ -81,6 +72,7 @@ export default {
   $planet-5-size: 100px;
   $space-ship-size: 128px;
   $robot-size: 250px;
+  $mathbot-header-size: 60px;
 
   #Marketing-top-section {
     grid-area: topsection;
@@ -101,6 +93,10 @@ export default {
     grid-column: c-two / span 2;
     grid-row: r-header;
     text-align: left;
+
+    .mathbot-header {
+      font-size: $mathbot-header-size;
+    }
   }
 
   .header-content {
@@ -203,17 +199,23 @@ export default {
     grid-row: r-four / span 3;
     z-index: 2;
     align-self: end;
-    max-width: 100vw;
   }
 
-  @media only screen and (max-width: 823px) {
-    $planet-1-size: 101px;
-    $planet-2-size: 32px;
-    $planet-3-size: 64px;
-    $planet-4-size: 45px;
-    $planet-5-size: 20px;
-    $space-ship-size: 34px;
-    $robot-size: 95px;
+  /* ipad pro Portrait */
+  @media only screen
+  and (min-device-width: 1024px) and (max-device-width: 1024px)
+  and (min-device-height: 1366px) and (max-device-height: 1366px)
+  and (min-width: 1024px) and (max-width: 1024px)
+  and (orientation: portrait)
+  and (-webkit-min-device-pixel-ratio: 1.5) {
+    $planet-1-size: 141px;
+    $planet-2-size: 72px;
+    $planet-3-size: 104px;
+    $planet-4-size: 85px;
+    $planet-5-size: 80px;
+    $space-ship-size: 74px;
+    $robot-size: 135px;
+    $mathbot-header-size: 50px;
 
     .marketing-container {
       padding: 50px 0 50px 0;
@@ -224,6 +226,10 @@ export default {
     .header-wrapper {
       text-align: left;
       margin-bottom: 10px;
+
+      .mathbot-header {
+        font-size: $mathbot-header-size;
+      }
     }
 
     .header-content {
@@ -312,14 +318,19 @@ export default {
     }
   }
 
-  @media only screen and (max-width: 736px) {
-    $planet-1-size: 101px;
-    $planet-2-size: 32px;
-    $planet-3-size: 64px;
-    $planet-4-size: 45px;
-    $planet-5-size: 20px;
-    $space-ship-size: 34px;
-    $robot-size: 95px;
+  // ipad landscape
+  @media only screen
+  and (min-device-width : 768px)
+  and (max-device-height : 1024px)
+  and (orientation : landscape) {
+    $planet-1-size: 141px;
+    $planet-2-size: 72px;
+    $planet-3-size: 104px;
+    $planet-4-size: 85px;
+    $planet-5-size: 80px;
+    $space-ship-size: 74px;
+    $robot-size: 135px;
+    $mathbot-header-size: 50px;
 
     .marketing-container {
       padding: 50px 0 50px 0;
@@ -330,6 +341,10 @@ export default {
     .header-wrapper {
       text-align: left;
       margin-bottom: 10px;
+
+      .mathbot-header {
+        font-size: $mathbot-header-size;
+      }
     }
 
     .header-content {
@@ -418,7 +433,344 @@ export default {
     }
   }
 
-  @media only screen and (max-width: 568px) {
+  // ipad portrait
+  @media only screen
+  and (min-device-width : 768px)
+  and (max-device-height: 1024px)
+  and (orientation : portrait)  {
+    $planet-1-size: 141px;
+    $planet-2-size: 72px;
+    $planet-3-size: 104px;
+    $planet-4-size: 85px;
+    $planet-5-size: 80px;
+    $space-ship-size: 74px;
+    $robot-size: 135px;
+    $mathbot-header-size: 50px;
+
+    .marketing-container {
+      padding: 50px 0 50px 0;
+      grid-template-columns: [c-one]12.5% [c-two]12.5% [c-three]12.5% [c-four]12.5% [c-five]12.5% [c-six]12.5% [c-seven]12.5% [c-eight]12.5%;
+      grid-template-rows: [r-header]auto [r-one]auto [r-two]auto [r-three]auto [r-four]auto [r-five]auto [r-six]auto [r-seven]auto [r-eight]auto [r-nine]auto;
+    }
+
+    .header-wrapper {
+      text-align: left;
+      margin-bottom: 10px;
+
+      .mathbot-header {
+        font-size: $mathbot-header-size;
+      }
+    }
+
+    .header-content {
+      text-align: left;
+      width: auto;
+      z-index: 4;
+      display: flex;
+      align-content: flex-end;
+      grid-column: c-two / span 6;
+      grid-row: r-two / span 3;
+    }
+
+    .slogan {
+      font-size: 18px;
+      line-height: 24px;
+      margin-bottom: 15px;
+    }
+
+    .minor-slogan {
+      font-size: 16px;
+      line-height: 20px;
+      margin-bottom: 59px;
+    }
+
+    .logo {
+      display: inline-block;
+      grid-column: c-six / span 2;
+      grid-row: r-three / span 2;
+      z-index: 4;
+      width: $robot-size;
+    }
+
+    .planet-one {
+      grid-column: c-five / span 2;
+      grid-row: r-one / span 3;
+      align-self: end;
+      justify-self: end;
+      height: $planet-1-size;
+    }
+
+    .planet-two {
+      grid-column: c-seven / span 2;
+      grid-row: r-four / span 2;
+      justify-self: center;
+      display: none;
+      height: $planet-2-size;
+    }
+
+    .planet-three {
+      grid-column: c-six / span 2;
+      grid-row: r-six / span 2;
+      justify-self: center;
+      align-self: start;
+      height: $planet-3-size;
+    }
+
+    .planet-four {
+      grid-column: c-two / span 2;
+      grid-row: r-five / span 2;
+      justify-self: center;
+      align-self: start;
+      margin-top: 30px;
+      height: $planet-4-size;
+    }
+
+    .planet-five {
+      grid-column: c-seven / span 2;
+      grid-row: r-header / span 2;
+      justify-self: center;
+      height: $planet-5-size;
+    }
+
+    .space-ship {
+      grid-column: c-four;
+      grid-row: r-six;
+      justify-self: end;
+      width: $space-ship-size;
+    }
+
+    .arched-line {
+      grid-column: c-one / span 8;
+      grid-row: r-four / span 3;
+      z-index: 2;
+      align-self: end;
+      max-width: 100vw;
+    }
+  }
+
+  @media only screen and (max-width: 823px) and (orientation: landscape) {
+    $planet-1-size: 141px;
+    $planet-2-size: 72px;
+    $planet-3-size: 104px;
+    $planet-4-size: 85px;
+    $planet-5-size: 80px;
+    $space-ship-size: 74px;
+    $robot-size: 135px;
+    $mathbot-header-size: 50px;
+
+    .marketing-container {
+      padding: 50px 0 50px 0;
+      grid-template-columns: [c-one]12.5% [c-two]12.5% [c-three]12.5% [c-four]12.5% [c-five]12.5% [c-six]12.5% [c-seven]12.5% [c-eight]12.5%;
+      grid-template-rows: [r-header]auto [r-one]auto [r-two]auto [r-three]auto [r-four]auto [r-five]auto [r-six]auto [r-seven]auto [r-eight]auto [r-nine]auto;
+    }
+
+    .header-wrapper {
+      text-align: left;
+      margin-bottom: 10px;
+
+      .mathbot-header {
+        font-size: $mathbot-header-size;
+      }
+    }
+
+    .header-content {
+      text-align: left;
+      width: auto;
+      z-index: 4;
+      display: flex;
+      align-content: flex-end;
+      grid-column: c-two / span 6;
+      grid-row: r-two / span 3;
+    }
+
+    .slogan {
+      font-size: 18px;
+      line-height: 24px;
+      margin-bottom: 15px;
+    }
+
+    .minor-slogan {
+      font-size: 16px;
+      line-height: 20px;
+      margin-bottom: 59px;
+    }
+
+    .logo {
+      display: inline-block;
+      grid-column: c-six / span 2;
+      grid-row: r-three / span 2;
+      z-index: 4;
+      width: $robot-size;
+    }
+
+    .planet-one {
+      grid-column: c-five / span 2;
+      grid-row: r-one / span 3;
+      align-self: end;
+      justify-self: end;
+      height: $planet-1-size;
+    }
+
+    .planet-two {
+      grid-column: c-seven / span 2;
+      grid-row: r-four / span 2;
+      justify-self: center;
+      display: none;
+      height: $planet-2-size;
+    }
+
+    .planet-three {
+      grid-column: c-six / span 2;
+      grid-row: r-six / span 2;
+      justify-self: center;
+      align-self: start;
+      height: $planet-3-size;
+    }
+
+    .planet-four {
+      grid-column: c-two / span 2;
+      grid-row: r-five / span 2;
+      justify-self: center;
+      align-self: start;
+      margin-top: 30px;
+      height: $planet-4-size;
+    }
+
+    .planet-five {
+      grid-column: c-seven / span 2;
+      grid-row: r-header / span 2;
+      justify-self: center;
+      height: $planet-5-size;
+    }
+
+    .space-ship {
+      grid-column: c-four;
+      grid-row: r-six;
+      justify-self: end;
+      width: $space-ship-size;
+    }
+
+    .arched-line {
+      grid-column: c-one / span 8;
+      grid-row: r-four / span 3;
+      z-index: 2;
+      align-self: end;
+      max-width: 100vw;
+    }
+  }
+
+  @media only screen and (max-width: 736px) and (orientation: landscape) {
+    $planet-1-size: 121px;
+    $planet-2-size: 52px;
+    $planet-3-size: 84px;
+    $planet-4-size: 65px;
+    $planet-5-size: 40px;
+    $space-ship-size: 54px;
+    $robot-size: 115px;
+    $mathbot-header-size: 40px;
+
+    .marketing-container {
+      padding: 50px 0 50px 0;
+      grid-template-columns: [c-one]12.5% [c-two]12.5% [c-three]12.5% [c-four]12.5% [c-five]12.5% [c-six]12.5% [c-seven]12.5% [c-eight]12.5%;
+      grid-template-rows: [r-header]auto [r-one]auto [r-two]auto [r-three]auto [r-four]auto [r-five]auto [r-six]auto [r-seven]auto [r-eight]auto [r-nine]auto;
+    }
+
+    .header-wrapper {
+      text-align: left;
+      margin-bottom: 10px;
+
+      .mathbot-header {
+        font-size: $mathbot-header-size;
+      }
+    }
+
+    .header-content {
+      text-align: left;
+      width: auto;
+      z-index: 4;
+      display: flex;
+      align-content: flex-end;
+      grid-column: c-two / span 6;
+      grid-row: r-two / span 3;
+    }
+
+    .slogan {
+      font-size: 18px;
+      line-height: 24px;
+      margin-bottom: 15px;
+    }
+
+    .minor-slogan {
+      font-size: 16px;
+      line-height: 20px;
+      margin-bottom: 59px;
+    }
+
+    .logo {
+      display: inline-block;
+      grid-column: c-six / span 2;
+      grid-row: r-three / span 2;
+      z-index: 4;
+      width: $robot-size;
+    }
+
+    .planet-one {
+      grid-column: c-five / span 2;
+      grid-row: r-one / span 3;
+      align-self: end;
+      justify-self: end;
+      height: $planet-1-size;
+    }
+
+    .planet-two {
+      grid-column: c-seven / span 2;
+      grid-row: r-four / span 2;
+      justify-self: center;
+      display: none;
+      height: $planet-2-size;
+    }
+
+    .planet-three {
+      grid-column: c-six / span 2;
+      grid-row: r-six / span 2;
+      justify-self: center;
+      align-self: start;
+      height: $planet-3-size;
+    }
+
+    .planet-four {
+      grid-column: c-two / span 2;
+      grid-row: r-five / span 2;
+      justify-self: center;
+      align-self: start;
+      margin-top: 30px;
+      height: $planet-4-size;
+    }
+
+    .planet-five {
+      grid-column: c-seven / span 2;
+      grid-row: r-header / span 2;
+      justify-self: center;
+      height: $planet-5-size;
+    }
+
+    .space-ship {
+      grid-column: c-four;
+      grid-row: r-six;
+      justify-self: end;
+      width: $space-ship-size;
+    }
+
+    .arched-line {
+      grid-column: c-one / span 8;
+      grid-row: r-four / span 3;
+      z-index: 2;
+      align-self: end;
+      max-width: 100vw;
+    }
+  }
+
+  @media only screen and (max-width: 667px) and (orientation: landscape) {
     $planet-1-size: 121px;
     $planet-2-size: 62px;
     $planet-3-size: 84px;
@@ -426,6 +778,7 @@ export default {
     $planet-5-size: 40px;
     $space-ship-size: 44px;
     $robot-size: 115px;
+    $mathbot-header-size: 30px;
 
     .marketing-container {
       padding: 50px 0 50px 0;
@@ -436,6 +789,122 @@ export default {
     .header-wrapper {
       text-align: left;
       margin-bottom: 10px;
+
+      .mathbot-header {
+        font-size: $mathbot-header-size;
+      }
+    }
+
+    .header-content {
+      text-align: left;
+      width: auto;
+      z-index: 4;
+      display: flex;
+      align-content: flex-end;
+      grid-column: c-two / span 6;
+      grid-row: r-two / span 3;
+    }
+
+    .slogan {
+      font-size: 18px;
+      line-height: 24px;
+      margin-bottom: 15px;
+    }
+
+    .minor-slogan {
+      font-size: 16px;
+      line-height: 20px;
+      margin-bottom: 59px;
+    }
+
+    .logo {
+      display: inline-block;
+      grid-column: c-six / span 2;
+      grid-row: r-three / span 2;
+      z-index: 4;
+      width: $robot-size;
+    }
+
+    .planet-one {
+      grid-column: c-five / span 2;
+      grid-row: r-one / span 3;
+      align-self: end;
+      justify-self: end;
+      height: $planet-1-size;
+    }
+
+    .planet-two {
+      grid-column: c-seven / span 2;
+      grid-row: r-four / span 2;
+      justify-self: center;
+      display: none;
+      height: $planet-2-size;
+    }
+
+    .planet-three {
+      grid-column: c-six / span 2;
+      grid-row: r-six / span 2;
+      justify-self: center;
+      align-self: start;
+      height: $planet-3-size;
+    }
+
+    .planet-four {
+      grid-column: c-two / span 2;
+      grid-row: r-five / span 2;
+      justify-self: center;
+      align-self: start;
+      margin-top: 30px;
+      height: $planet-4-size;
+    }
+
+    .planet-five {
+      grid-column: c-seven / span 2;
+      grid-row: r-header / span 2;
+      justify-self: center;
+      height: $planet-5-size;
+    }
+
+    .space-ship {
+      grid-column: c-four;
+      grid-row: r-six;
+      justify-self: end;
+      width: $space-ship-size;
+    }
+
+    .arched-line {
+      grid-column: c-one / span 8;
+      grid-row: r-four / span 3;
+      z-index: 2;
+      align-self: end;
+      max-width: 100vw;
+    }
+  }
+
+  @media only screen and (max-width: 568px) and (orientation: landscape) {
+    $planet-1-size: 121px;
+    $planet-2-size: 62px;
+    $planet-3-size: 84px;
+    $planet-4-size: 65px;
+    $planet-5-size: 40px;
+    $space-ship-size: 44px;
+    $robot-size: 115px;
+    $mathbot-header-size: 30px;
+
+    .marketing-container {
+      padding: 50px 0 50px 0;
+      grid-template-columns: [c-one]12.5% [c-two]12.5% [c-three]12.5% [c-four]12.5% [c-five]12.5% [c-six]12.5% [c-seven]12.5% [c-eight]12.5%;
+      grid-template-rows: [r-header]auto [r-one]auto [r-two]auto [r-three]auto [r-four]auto [r-five]auto [r-six]auto [r-seven]auto [r-eight]auto [r-nine]auto;
+    }
+
+    .header-wrapper {
+      text-align: left;
+      margin-bottom: 10px;
+
+      .mathbot-header {
+        font-size: $mathbot-header-size;
+        margin-bottom: 20px;
+      }
     }
 
     .header-content {
@@ -532,6 +1001,7 @@ export default {
     $planet-5-size: 20px;
     $space-ship-size: 34px;
     $robot-size: 95px;
+    $mathbot-header-size: 30px;
 
     .marketing-container {
       padding: 50px 0 50px 0;
@@ -542,6 +1012,11 @@ export default {
     .header-wrapper {
       text-align: left;
       margin-bottom: 10px;
+
+      .mathbot-header {
+        font-size: $mathbot-header-size;
+        margin-bottom: 20px;
+      }
     }
 
     .header-content {
@@ -638,6 +1113,7 @@ export default {
     $planet-5-size: 20px;
     $space-ship-size: 34px;
     $robot-size: 95px;
+    $mathbot-header-size: 30px;
 
     .marketing-container {
       padding: 50px 0 50px 0;
@@ -648,6 +1124,11 @@ export default {
     .header-wrapper {
       text-align: left;
       margin-bottom: 10px;
+
+      .mathbot-header {
+        font-size: $mathbot-header-size;
+        margin-bottom: 20px;
+      }
     }
 
     .header-content {
@@ -744,6 +1225,7 @@ export default {
     $planet-5-size: 20px;
     $space-ship-size: 34px;
     $robot-size: 95px;
+    $mathbot-header-size: 30px;
 
     .marketing-container {
       padding: 50px 0 50px 0;
@@ -754,6 +1236,11 @@ export default {
     .header-wrapper {
       text-align: left;
       margin-bottom: 10px;
+
+      .mathbot-header {
+        font-size: $mathbot-header-size;
+        margin-bottom: 20px;
+      }
     }
 
     .header-content {
