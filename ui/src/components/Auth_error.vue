d3eae18e278b4a0cf9b2b09c828f58436dfd3333
Richard Porter <gifted.primate@protonmail.com>
2018-09-14 16:51:51 -0400

    WIP: adds auth error component

---
+++ ui/src/components/Auth_error.vue
@@ -0,0 +1,19 @@
+<template>
+ <div class="auth-error">
+   <span>{{errorMessage}}</span>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Auth_error',
+  props: ['errorMessage']
+}
+</script>
+
+<style scoped lang="scss">
+$error-background: #FF4500;
+.auth-error {
+  background-color: $error-background;
+}
+</style>
