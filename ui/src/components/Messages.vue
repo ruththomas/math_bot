4216f29dbbcae95d7d731732d182c92a30821b7d
Richard Porter <gifted.primate@protonmail.com>
2018-09-27 09:10:07 -0400

    optimizes messages for all device sizes

--- ui/src/components/Messages.vue
+++ ui/src/components/Messages.vue
@@ -29,9 +29,10 @@ export default {
 <style scoped lang="scss">
   .message-container {
     position: fixed;
-    right: 5px;
-    top: 10px;
-    width: 181px;
+    top: 1vh;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 30vmin;
     z-index: 1001;
     display: flex;
     flex-direction: column;
@@ -42,90 +43,42 @@ export default {
     display: flex;
     width: 99%;
     margin-top: 8px;
-    min-height: 50px;
     border-radius: 2px;
     justify-content: center;
     align-items: center;
-    font-size: 14px;
-    line-height: 16px;
+    font-size: 2vmin;
+    line-height: 4vmin;
     cursor: pointer;
-    padding: 20px;
   }
 
   .info {
     border: 1px solid rgb(0, 0, 255);
-    background-color: rgba(0, 0, 255, 0.2);
+    background-color: rgba(0, 0, 255, 0.5);
   }
 
   .success {
-    background-color: rgba(184,233,134,0.2);
+    background-color: rgba(184,233,134,0.5);
     border: 1px solid rgb(184, 233, 134);
   }
 
   .warn {
     border: 1px solid rgb(255, 0, 0);
-    background-color: rgba(255, 0, 0, 0.2);
+    background-color: rgba(255, 0, 0, 0.5);
   }
 
   .message:nth-child(n+2) {
     opacity: 0.5;
   }
 
-  .close-message {
-    width: 20px;
-    height: 20px;
-    position: relative;
-    border-radius: 2px;
-    float: left;
+  .message:nth-child(n+3) {
+    opacity: 0.4;
   }
 
-  .close-message::after, .close-message::before{
-    position: absolute;
-    top: 9px;
-    left: 0px;
-    content: '';
-    display: block;
-    width: 20px;
-    height: 2px;
-    background-color: #FFFFFF;
+  .message:nth-child(n+4) {
+    opacity: 0.3;
   }
 
-  .close-message::after {
-    transform: rotate(45deg);
+  .message:nth-child(n+5) {
+    opacity: 0.2;
   }
-
-  .close-message::before {
-    transform: rotate(-45deg);
-  }
-
-  /* Large Phones, landscape*/
-  @media only screen and (max-width : 992px) {
-    .message-container {
-      width: 200px;
-      right: 10px;
-    }
-
-    .message {
-      height: 20px;
-      font-size: 10px;
-    }
-  }
-
-  /* Small Devices */
-  @media only screen and (max-width : 667px) {
-    .message-container {
-      display: none;
-    }
-  }
-
-  /* Extra Small Devices, Phones */
-  @media only screen and (max-width : 480px) {
-
-  }
-
-  /* Custom, iPhone 5 Retina */
-  @media only screen and (max-width : 320px) {
-
-  }
-
 </style>
