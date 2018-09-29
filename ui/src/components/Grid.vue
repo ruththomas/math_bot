da00b0f8644ad21dd5f879e27982b13b059e941b
Richard Porter <gifted.primate@protonmail.com>
2018-09-29 11:29:36 -0400

    generated functions added if the user didn't use them on a previous level. minor improvements to game styles

--- ui/src/components/Grid.vue
+++ ui/src/components/Grid.vue
@@ -1,80 +1,76 @@
 <template>
     <div class="container grid">
-      <control-panel></control-panel>
       <video-hint v-if="hintShowing.showing" key="video-hint-1234"></video-hint>
-      <transition
-        mode="out-in"
-        name="grid-transition-group"
-        enter-active-class="animated zoomIn"
-        leave-active-class="animated zoomOut"
-      >
-        <congrats v-if="congratsShowing" :congrats="true" key="congrats-1234"></congrats>
-        <congrats v-else-if="tryAgainShowing" :congrats="false" key="congrats-5678"></congrats>
-        <div v-else-if="gridMap" class="row" :class="robotCarrying.length ? 'no-radius-bottom-right' : ''" key="grid-map-1234">
-          <div class="grid-map">
+      <div v-else-if="gridMap" class="row" :class="robotCarrying.length ? 'no-radius-bottom-right' : ''" key="grid-map-1234">
+        <div class="grid-map">
+          <control-panel></control-panel>
+          <div
+            class="grid-row animated"
+            v-for="(row, rInd) in gridMap"
+            :key="'row:' + rInd"
+          >
             <div
-              class="grid-row animated"
-              v-for="(row, rInd) in gridMap"
-              :key="'row:' + rInd"
+              class="grid-space animated"
+              v-for="(space, sInd) in row"
+              :id="`grid-cell-${rInd}-${sInd}`"
+              :class="[
+                'grid-space-' + space.name.replace(/ /g, '-'),
+                !rInd ? 'no-border-top': '',
+                rInd === gridMap.length - 1 ? 'no-border-bottom' : '',
+                !sInd ? 'no-border-left' : '',
+                sInd === row.length - 1 ? 'no-border-right' : ''
+              ]"
+              :key="'space:' + rInd + ':' + sInd"
             >
-              <div
-                class="grid-space animated"
-                v-for="(space, sInd) in row"
-                :id="`grid-cell-${rInd}-${sInd}`"
-                :class="'grid-space-' + space.name.replace(/ /g, '-')"
-                :key="'space:' + rInd + ':' + sInd"
-              >
-                <span v-if="space.name === 'final answer'"
-                      class="problem single-digit-problem">{{singleDigitProblem(problem)}}</span>
-                <b-img
-                  v-if="space.name === 'final answer'"
-                  class="portal glyphicon"
-                  :src="permanentImages.blackHole"></b-img>
-                <b-img
-                  v-if="space.tools.length"
-                  class="tool animated zoomIn"
-                  v-for="(tool, tInd) in space.tools"
-                  :key="'tool:' + tInd + ':' + rInd + ':' + sInd"
-                  :src="toolImages[tool.image]"></b-img>
-                <b-img
-                  class="robot animated"
-                  v-if="robot.robotLocation.x === rInd && robot.robotLocation.y === sInd"
-                  :key="'ROBOT'"
-                  :src="robot._robotDirections[robotOrientation]"></b-img>
+              <span v-if="space.name === 'final answer'"
+                    class="problem single-digit-problem">{{singleDigitProblem(problem)}}</span>
+              <b-img
+                v-if="space.name === 'final answer'"
+                class="portal glyphicon"
+                :src="permanentImages.blackHole"></b-img>
+              <b-img
+                v-if="space.tools.length"
+                class="tool animated zoomIn"
+                v-for="(tool, tInd) in space.tools"
+                :key="'tool:' + tInd + ':' + rInd + ':' + sInd"
+                :src="toolImages[tool.image]"></b-img>
+              <b-img
+                class="robot animated"
+                v-if="robot.robotLocation.x === rInd && robot.robotLocation.y === sInd"
+                :key="'ROBOT'"
+                :src="robot._robotDirections[robotOrientation]"></b-img>
 
-                <b-popover
-                  v-if="space.tools.length"
-                  :target="`grid-cell-${rInd}-${sInd}`"
-                  placement="auto"
-                  triggers="click"
-                >
-                  <img class="dialog-button close-popover" :src="permanentImages.buttons.xButton" @click="closePopover(`grid-cell-${rInd}-${sInd}`)" />
-                  <div class="display-tools">
-                    <div
-                      v-for="(tool, iInd) in space.tools.slice(0, 100)"
-                      :key="`d-image-${iInd}`"
-                      :class="tool.original ? 'replenish-tool' : ''"
-                    >
-                      <b-img
-                        :src="permanentImages.tools[tool.image]"
-                        fluid
-                      ></b-img>
-                    </div>
+              <b-popover
+                v-if="space.tools.length"
+                :target="`grid-cell-${rInd}-${sInd}`"
+                placement="auto"
+                triggers="click"
+              >
+                <img class="dialog-button close-popover" :src="permanentImages.buttons.xButton" @click="closePopover(`grid-cell-${rInd}-${sInd}`)" />
+                <div class="display-tools">
+                  <div
+                    v-for="(tool, iInd) in space.tools.slice(0, 100)"
+                    :key="`d-image-${iInd}`"
+                    :class="tool.original ? 'replenish-tool' : ''"
+                  >
+                    <b-img
+                      :src="permanentImages.tools[tool.image]"
+                      fluid
+                    ></b-img>
                   </div>
-                </b-popover>
-              </div>
-              <RobotCarrying></RobotCarrying>
+                </div>
+              </b-popover>
             </div>
+            <RobotCarrying></RobotCarrying>
           </div>
         </div>
-        <splash-screen v-else></splash-screen>
-      </transition>
+      </div>
+      <splash-screen v-else></splash-screen>
     </div>
 </template>
 
 <script>
 import assets from '../assets/assets'
-import Congrats from './Congrats'
 import SplashScreen from './Splash_screen'
 import RobotCarrying from './Robot_carrying'
 import _ from 'underscore'
@@ -122,12 +118,6 @@ export default {
     },
     robot () {
       return this.$store.getters.getRobot
-    },
-    congratsShowing () {
-      return this.$store.getters.getCongratsShowing
-    },
-    tryAgainShowing () {
-      return this.$store.getters.getTryAgainShowing
     },
     permanentImages () {
       return this.$store.getters.getPermanentImages
@@ -192,7 +182,6 @@ export default {
     closePopover: utils.closePopover
   },
   components: {
-    Congrats,
     SplashScreen,
     RobotCarrying,
     ControlPanel,
@@ -214,71 +203,63 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
     color: #ffffff;
 
     .grid-map {
       position: relative;
-      border: 1px solid $click-color;
       border-radius: $grid-border-radius 0 $grid-border-radius $grid-border-radius;
     }
   }
 
   .grid-row {
     display: flex;
-    &:first-child .grid-space:first-child {
-      border-top-left-radius: $grid-border-radius;
+
+    .grid-space {
+      position: relative;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: $grid-space-size;
+      width: $grid-space-size;
+      border-top: 1px solid rgba(255, 255, 255, 0.2);
+      border-right: 1px solid rgba(255, 255, 255, 0.2);
+      font-size: $grid-space-font-size;
+      background: $grid-background;
+
+      img {
+        position: absolute;
+        height: 90%;
+      }
+
+      img.tool {
+        cursor: pointer;
+        height: 75%;
+        width: 75%;
+      }
+
+      img.robot {
+        height: 150%;
+        top: -35%;
+        z-index: 9;
+      }
+
+      &.no-border-top {
+        border-top: none;
+      }
+
+      &.no-border-right {
+        border-right: none;
+      }
+
+      &.no-border-bottom {
+        border-bottom: none;
+      }
+
+      &.no-border-left {
+        border-left: none;
+      }
     }
-
-    &:first-child .grid-space:last-child {
-      border-top-right-radius: $grid-border-radius;
-    }
-
-    &:last-child .grid-space:first-child {
-      border-bottom-left-radius: $grid-border-radius;
-    }
-
-    &:last-child .grid-space:last-child {
-      border-bottom-right-radius: $grid-border-radius;
-    }
-  }
-
-  .no-radius-bottom-right {
-    border-bottom-right-radius: 0;
-
-    &:last-child .grid-space:last-child {
-      border-bottom-right-radius: 0;
-    }
-  }
-
-  .grid-space {
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: $grid-space-size;
-    width: $grid-space-size;
-    border-top: 1px solid rgba(255, 255, 255, 0.2);
-    border-right: 1px solid rgba(255, 255, 255, 0.2);
-    font-size: $grid-space-font-size;
-    background: $grid-background;
-
-    img {
-      position: absolute;
-      height: 90%;
-    }
-
-    img.tool {
-      cursor: pointer;
-      height: 75%;
-      width: 75%;
-    }
-
-    img.robot {
-      height: 150%;
-      top: -35%;
-      z-index: 9;
-    }
-
   }
 
   .display-tools {
@@ -316,14 +297,6 @@ export default {
         rgba(74, 74, 74, 1) calc(25% + 0.75px),
         rgba(74, 74, 74, 1) calc(25% - 5px)
     )
-  }
-
-  .grid-row:nth-child(5n+0) .grid-space {
-    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
-  }
-
-  .grid-space:nth-child(1) {
-    border-left: 1px solid rgba(255, 255, 255, 0.2);
   }
 
   .robot-shake {
