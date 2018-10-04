c2136d80ff804e1eb3a692fc46931b8d24b4ee55
Richard Porter <gifted.primate@protonmail.com>
2018-10-04 17:50:48 -0400

    adds tell your friends to level congrats and step congrats

--- ui/src/components/Step_congrats.vue
+++ ui/src/components/Step_congrats.vue
@@ -12,6 +12,9 @@
     </div>
     <stars :level="level" :step="step" :step-stats="stepStats" :star-group="'congrats-spread'"></stars>
     <div class="text-minor">You won!</div>
+    <div class="text-minor">
+      <div>Tell your friends!</div>
+    </div>
     <social-sharing :message="socialMessage" :size="'3rem'"></social-sharing>
     <div slot="modal-footer" class="row" style="width: 100%; display: flex; justify-content: space-between;">
       <b-btn
@@ -112,6 +115,9 @@ export default {
 
     .text-minor {
       font-size: $font-size;
+      *:first-child {
+        font-size: calc(#{$font-size} / 2);
+      }
     }
 
     .modal-dialog .modal-content {
