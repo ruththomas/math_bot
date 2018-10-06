0ae05af5dc86b2158d11bdc85014c780d0503347
Jaime Martinez <jaimefm@yahoo.com>
2018-10-05 23:54:40 -0700

    Adding animation of robot mascot. cycles through 4 animations at random intervals.

---
+++ ui/src/components/Mascot.vue
@@ -0,0 +1,345 @@
+<template>
+<div class ="splash-screen">
+
+<svg width="146px" height="213px" viewBox="0 0 146 213" version="1.1" xmlns="http://www.w3.org/2000/svg">
+  <g class="InstructionRobot" fill-rule="evenodd" stroke="none" stroke-width="1" fill="#000000">
+    <path class="HeadBody" d="m 125.42434,60.271276 c 0,-6.445466 -5.24443,-11.68989 -11.68989,-11.68989 H 78.526622 C 76.844795,47.045085 74.794551,45.9122 72.521415,45.326416 h 0.07921 v -6.064153 c 5.382581,-1.563933 9.332017,-6.532044 9.332017,-12.411989 0,-7.130722 -5.802577,-12.933299 -12.933299,-12.933299 -7.130723,0 -12.9333,5.802577 -12.9333,12.933299 0,5.879945 3.949437,10.848056 9.332018,12.411989 v 6.064153 h 0.07921 c -2.273137,0.585784 -4.323381,1.716826 -6.005207,3.25497 H 24.413446 c -6.445466,0 -11.689891,5.244424 -11.689891,11.68989 v 6.7052 l -7.2523,2.67287 -4.7102189,1.737089 v 5.01969 17.805622 5.021532 l 4.7102189,1.737091 7.2523,2.67287 v 6.7052 c 0,6.44546 5.244425,11.68989 11.689891,11.68989 h 10.969633 v 2.61945 3.51654 c -10.411481,3.8997 -18.500089,12.87251 -20.937171,24.17372 -5.6515263,1.61183 -9.8238555,7.01836 -9.8238555,13.44172 H 13.73302 c 0,-2.66366 1.884455,-4.8318 4.198118,-4.8318 2.315505,0 4.199961,2.16814 4.199961,4.8318 h 9.110967 c 0,-6.26863 -3.971542,-11.58305 -9.416754,-13.33119 1.998665,-7.27809 7.036776,-13.17462 13.555925,-16.43143 v 19.77482 7.20072 h 7.200722 52.831083 7.200718 v -7.20072 -19.77113 c 6.51547,3.25681 11.55174,9.15149 13.55224,16.42774 -5.44337,1.74998 -9.41306,7.06256 -9.41306,13.33119 h 9.11096 c 0,-2.66366 1.88446,-4.8318 4.19996,-4.8318 2.31551,0 4.19812,2.16814 4.19812,4.8318 h 9.11097 c 0,-6.42336 -4.17417,-11.83173 -9.8257,-13.44172 -2.44076,-11.29752 -10.52753,-20.26665 -20.93349,-24.16819 v -3.52023 -2.62129 h 11.11885 c 6.44546,0 11.68989,-5.24443 11.68989,-11.68989 v -6.76046 l 7.10309,-2.61761 4.71022,-1.737091 v -5.021532 -17.805622 -5.01969 l -4.71022,-1.737089 -7.10309,-2.617607 V 60.271276 Z M 12.723555,95.967257 7.9617579,94.211747 V 76.406125 l 4.7617971,-1.75551 z M 63.268607,26.850274 c 0,-3.164707 2.566029,-5.730736 5.730736,-5.730736 3.164707,0 5.730735,2.566029 5.730735,5.730736 0,3.164707 -2.566028,5.732578 -5.730735,5.732578 -3.164707,0 -5.730736,-2.566029 -5.730736,-5.732578 z M 95.414884,155.80012 H 42.583801 v -31.14234 h 52.831083 z m 22.808736,-45.45168 c 0,2.47945 -2.00972,4.48732 -4.48733,4.48732 H 24.413446 c -2.479451,0 -4.487327,-2.00971 -4.487327,-4.48732 V 60.271276 c 0,-2.47945 2.009718,-4.487326 4.487327,-4.487326 h 89.322844 c 2.47945,0 4.48733,2.009718 4.48733,4.487326 z m 11.81331,-33.942315 v 17.805622 l -4.61259,1.700247 V 74.705877 Z"/>
+    <circle class="DoorHandle" cx="87" cy="141" r="4"  fill="none" stroke="#000000" stroke-width="3"/>
+    <path class="Visor" d="m 30.36339,91.185196 c 0,7.213617 5.868892,13.080664 13.082508,13.080664 h 51.108731 c 7.213621,0 13.080671,-5.868889 13.080671,-13.080664 V 66.352008 H 30.36339 Z m 4.389695,-20.443492 h 68.490675 v 20.443492 c 0,4.791271 -3.89786,8.690971 -8.690973,8.690971 H 43.445898 c -4.79127,0 -8.690971,-3.897858 -8.690971,-8.690971 V 70.741704 Z"/>
+    <g class="Eyes">
+       <path class="RightEye" d="m 90.155723,93.624121 h 1.707615 c 2.630502,0 4.771008,-2.140506 4.771008,-4.77285 V 82.116597 H 93.922793 92.39386 V 78.478474 75.7135 c -0.176841,-0.02026 -0.348155,-0.05342 -0.528679,-0.05342 h -1.707616 c -2.630502,0 -4.77285,2.140506 -4.77285,4.77285 v 8.418342 c -0.0018,2.632344 2.138664,4.77285 4.771008,4.772849 z"/>
+       <path class="LeftEye" d="m 46.135347,93.025442 h 1.980244 c 2.630502,0 4.77285,-2.140506 4.77285,-4.77285 v -6.134153 h -2.711553 -2.866289 v -3.744964 -2.711554 h -1.177094 c -2.630502,0 -4.77285,2.140506 -4.77285,4.77285 v 7.817821 c 0.0018,2.630502 2.142348,4.77285 4.774692,4.77285 z"/>
+    </g>
+    <g class="LightLines">
+       <path class="LightLines-Line-1" d="m 46.627184,5.2333719 -4.124435,2.3818197 4.936796,8.5509724 c 1.318935,-0.876834 2.696817,-1.670774 4.122593,-2.38182 z"/>
+       <path class="LightLines-Line-2" d="m 70.92064,0 h -4.761797 v 9.8791178 c 0.788414,-0.047894 1.580512,-0.081052 2.379978,-0.081052 0.801308,0 1.593406,0.033158 2.379977,0.081052 V 0 Z"/>
+       <path class="LightLines-Line-3" d="m 90.452299,5.2333719 -4.936796,8.5509721 c 1.427619,0.711046 2.8055,1.504986 4.122593,2.38182 l 4.936796,-8.5509724 z"/>
+    </g>
+    <path class="LeftLeg" d="m 74.497744,213.03908 c 0,-0.11001 -2.235706,-6.038 -4.968234,-13.1733 l -4.968237,-12.97328 -0.350085,-5.43229 c -0.393242,-6.10205 -1.311349,-8.57393 -4.291328,-11.5539 -2.758022,-2.75804 -5.685592,-3.90216 -10.020673,-3.9162 -5.787349,-0.0187 -9.993766,2.26068 -12.702989,6.88362 -1.245826,2.12584 -1.531118,3.39908 -1.852166,8.26584 l -0.3795,5.75293 -4.679923,12.22264 c -2.57396,6.72247 -4.81807,12.65045 -4.986918,13.17331 l -0.306994,0.95065 h 24.753524 c 13.614435,0 24.753523,-0.09 24.753523,-0.20002 z M 36.294768,202.51032 c 0.772228,-2.01673 1.785747,-4.82794 2.252261,-6.24712 l 0.848208,-2.58034 h 10.130464 10.13047 l 0.691932,1.49387 c 0.380565,0.82164 1.004394,2.34947 1.386284,3.39519 0.381889,1.04572 1.206228,3.18468 1.831864,4.75325 l 1.137517,2.85195 H 49.797246 34.890718 Z m 5.621171,-19.28462 c 0.02092,-6.90147 2.646986,-10.36101 7.864885,-10.36101 5.217898,0 7.843986,3.45954 7.864895,10.36101 l 0.01195,3.93841 h -7.876822 -7.876818 z"/>
+    <path class="RightLeg" d="m 63.495487,213.00515 c 0,-0.11001 2.2357,-6.038 4.96823,-13.1733 l 4.96824,-12.97328 0.35008,-5.43229 c 0.39324,-6.10205 1.31135,-8.57393 4.29133,-11.5539 2.758022,-2.75804 5.685592,-3.90216 10.020673,-3.9162 5.787349,-0.0187 9.993766,2.26068 12.70299,6.88362 1.24582,2.12584 1.53112,3.39908 1.85216,8.26584 l 0.3795,5.75293 4.67993,12.22264 c 2.57396,6.72247 4.81807,12.65045 4.98692,13.17331 l 0.30699,0.95065 H 88.249006 c -13.614439,0 -24.753519,-0.09 -24.753519,-0.20002 z m 38.202973,-10.52876 c -0.77223,-2.01673 -1.785748,-4.82794 -2.252262,-6.24712 L 98.59799,193.64893 H 88.467526 78.337056 l -0.691932,1.49387 c -0.380565,0.82164 -1.004394,2.34947 -1.386287,3.39519 -0.38189,1.04572 -1.20622,3.18468 -1.83186,4.75325 l -1.13752,2.85195 h 14.906524 14.906529 z m -5.621172,-19.28462 c -0.02092,-6.90147 -2.646986,-10.36101 -7.864885,-10.36101 -5.217898,0 -7.843986,3.45954 -7.864895,10.36101 l -0.01195,3.93841 h 7.876822 7.876818 z"/>
+  </g>
+</svg>
+    </div>
+</template>
+
+<script>
+
+var existingAnimation = ['eyes-up-down', 'eyes-left-right', 'blink-eyes', 'lights']
+var animationClass = ['.Eyes', '.Eyes', '.Eyes', '.LightLines']
+
+var minimumDelay = 2000
+var variableDelay = 8000
+
+function selectAnimation (previousAnimation) {
+  var selectedAnimation
+  do {
+    selectedAnimation = Math.floor(Math.random() * existingAnimation.length)
+  }
+  while (selectedAnimation === previousAnimation)
+
+  return selectedAnimation
+}
+
+function getRandomInterval () {
+  return Math.round(Math.random() * variableDelay) + minimumDelay
+}
+
+function addClassToHtml (animationNumber) {
+  if (animationNumber === (existingAnimation.length - 1)) {
+    $('.LightLines-Line-1').addClass('line-1')
+    $('.LightLines-Line-2').addClass('line-2')
+    $('.LightLines-Line-3').addClass('line-3')
+  } else {
+    $(animationClass[animationNumber]).addClass(existingAnimation[animationNumber])
+  }
+}
+
+function removeClassFromHtml (animationNumber) {
+  if (animationNumber === (existingAnimation.length - 1)) {
+    $('.LightLines-Line-1').removeClass('line-1')
+    $('.LightLines-Line-2').removeClass('line-2')
+    $('.LightLines-Line-3').removeClass('line-3')
+  } else {
+    $(animationClass[animationNumber]).removeClass(existingAnimation[animationNumber])
+  }
+}
+
+function reloadAnimation (previousAnimation, nextAnimation) {
+  removeClassFromHtml(previousAnimation)
+  addClassToHtml(nextAnimation)
+}
+
+function loop (previousAnimation) {
+  var currentAnimation = selectAnimation(previousAnimation)
+
+  setTimeout(function () {
+    reloadAnimation(previousAnimation, currentAnimation)
+    loop(currentAnimation)
+  }, getRandomInterval())
+};
+
+setTimeout(function () {
+  var firstAnimation = selectAnimation(-1)
+  reloadAnimation(-1, firstAnimation)
+  loop(firstAnimation)
+}, minimumDelay)
+
+</script>
+
+<style scoped lang="scss">
+
+.splash-screen {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+}
+
+@keyframes move-eyes-up-down {
+    0%, 20% { transform: none;
+              transform-origin: 68.145535px 90.025442px 10px;
+    }
+    30%, 40% { transform: rotateX(-32deg);
+               transform-origin: 68.145535px 90.025442px 10px;
+    }
+    60%, 70% { transform: rotateX(32deg);
+               transform-origin: 68.145535px 90.025442px 10px;
+    }
+    80%, 100% { transform: none;
+                transform-origin: 68.145535px 90.025442px 10px;
+    }
+
+}
+
+@-webkit-keyframes move-eyes-up-down {
+    0%, 20% { -webkit-transform: none;
+              -webkit-transform-origin: 68.145535px 90.025442px 10px;
+    }
+    30%, 40% { -webkit-transform: rotateX(-32deg);
+               -webkit-transform-origin: 68.145535px 90.025442px 10px;
+    }
+    60%, 70% { -webkit-transform: rotateX(32deg);
+               -webkit-transform-origin: 68.145535px 90.025442px 10px;
+    }
+    80%, 100% { -webkit-transform: none;
+                -webkit-transform-origin: 68.145535px 90.025442px 10px;
+    }
+
+}
+
+@-moz-keyframes move-eyes-up-down {
+    0%, 20% { -moz-transform: none;
+              -moz-transform-origin: 68.145535px 90.025442px 10px;
+    }
+    30%, 40% { -moz-transform: rotateX(-32deg);
+               -moz-transform-origin: 68.145535px 90.025442px 10px;
+    }
+    60%, 70% { -moz-transform: rotateX(32deg);
+               -moz-transform-origin: 68.145535px 90.025442px 10px;
+    }
+    80%, 100% { -moz-transform: none;
+                -moz-transform-origin: 68.145535px 90.025442px 10px;
+    }
+
+}
+
+@-o-keyframes move-eyes-up-down {
+    0%, 20% { -o-transform: none;
+              -o-transform-origin: 68.145535px 90.025442px 10px;
+    }
+    30%, 40% { -o-transform: rotateX(-32deg);
+               -o-transform-origin: 68.145535px 90.025442px 10px;
+    }
+    60%, 70% { -o-transform: rotateX(32deg);
+               -o-transform-origin: 68.145535px 90.025442px 10px;
+    }
+    80%, 100% { -o-transform: none;
+                -o-transform-origin: 68.145535px 90.025442px 10px;
+    }
+
+}
+
+@keyframes move-eyes-left-right {
+    0%, 10% { transform: none;
+              transform-origin: 68.145535px 84.025442px 15px;
+    }
+    20%, 30% { transform: rotateY(-25deg);
+               transform-origin: 68.145535px 84.025442px 15px;
+    }
+    50%, 60% { transform: rotateY(25deg);
+               transform-origin: 68.145535px 84.025442px 15px;
+    }
+    70%, 100% { transform: none;
+                transform-origin: 68.145535px 84.025442px 15px;
+    }
+}
+
+@-webkit-keyframes move-eyes-left-right {
+    0%, 10% { -webkit-transform: none;
+              -webkit-transform-origin: 68.145535px 84.025442px 15px;
+    }
+    20%, 30% { -webkit-transform: rotateY(-25deg);
+               -webkit-transform-origin: 68.145535px 84.025442px 15px;
+    }
+    50%, 60% { -webkit-transform: rotateY(25deg);
+               -webkit-transform-origin: 68.145535px 84.025442px 15px;
+    }
+    70%, 100% { -webkit-transform: none;
+                -webkit-transform-origin: 68.145535px 84.025442px 15px;
+    }
+}
+
+@-moz-keyframes move-eyes-left-right {
+    0%, 10% { -moz-transform: none;
+              -moz-transform-origin: 68.145535px 84.025442px 15px;
+    }
+    20%, 30% { -moz-transform: rotateY(-25deg);
+               -moz-transform-origin: 68.145535px 84.025442px 15px;
+    }
+    50%, 60% { -moz-transform: rotateY(25deg);
+               -moz-transform-origin: 68.145535px 84.025442px 15px;
+    }
+    70%, 100% { -moz-transform: none;
+                -moz-transform-origin: 68.145535px 84.025442px 15px;
+    }
+}
+
+@-o-keyframes move-eyes-left-right {
+    0%, 10% { -o-transform: none;
+              -o-transform-origin: 68.145535px 84.025442px 15px;
+    }
+    20%, 30% { -o-transform: rotateY(-25deg);
+               -o-transform-origin: 68.145535px 84.025442px 15px;
+    }
+    50%, 60% { -o-transform: rotateY(25deg);
+               -o-transform-origin: 68.145535px 84.025442px 15px;
+    }
+    70%, 100% { -o-transform: none;
+                -o-transform-origin: 68.145535px 84.025442px 15px;
+    }
+}
+
+@keyframes blink-both-eyes {
+    0%, 5% { transform: none;
+             transform-origin: 68.145535px 84.025442px 15px;
+    }
+    9%, 15% { transform:scaleY(0.15);
+              transform-origin: 68.145535px 84.025442px 15px;
+    }
+    20%, 100% { transform: none;
+                transform-origin: 68.145535px 84.025442px 15px;
+    }
+}
+
+@-webkit-keyframes blink-both-eyes {
+    0%, 5% { -webkit-transform: none;
+             -webkit-transform-origin: 68.145535px 84.025442px 15px;
+    }
+    9%, 15% { -webkit-transform:scaleY(0.15);
+              -webkit-transform-origin: 68.145535px 84.025442px 15px;
+    }
+    20%, 100% { -webkit-transform: none;
+                -webkit-transform-origin: 68.145535px 84.025442px 15px;
+    }
+}
+
+@-moz-keyframes blink-both-eyes {
+    0%, 5% { -moz-transform: none;
+             -moz-transform-origin: 68.145535px 84.025442px 15px;
+    }
+    9%, 15% { -moz-transform:scaleY(0.15);
+              -moz-transform-origin: 68.145535px 84.025442px 15px;
+    }
+    20%, 100% { -moz-transform: none;
+                -moz-transform-origin: 68.145535px 84.025442px 15px;
+    }
+}
+
+@-o-keyframes blink-both-eyes {
+    0%, 5% { -o-transform: none;
+             -o-transform-origin: 68.145535px 84.025442px 15px;
+    }
+    9%, 15% { -o-transform:scaleY(0.15);
+              -o-transform-origin: 68.145535px 84.025442px 15px;
+    }
+    20%, 100% { -o-transform: none;
+                -o-transform-origin: 68.145535px 84.025442px 15px;
+    }
+}
+
+@keyframes LightLines {
+  0% {opacity: 1}
+  8%, 13% {opacity: 0}
+  20%, 100% {opacity: 1}
+}
+
+@-webkit-keyframes LightLines {
+  0% {opacity: 1}
+  8%, 13% {opacity: 0}
+  20%, 100% {opacity: 1}
+}
+
+@-moz-keyframes LightLines {
+  0% {opacity: 1}
+  8%, 13% {opacity: 0}
+  20%, 100% {opacity: 1}
+}
+
+@-o-keyframes LightLines {
+  0% {opacity: 1}
+  8%, 13% {opacity: 0}
+  20%, 100% {opacity: 1}
+}
+
+.eyes-up-down {
+  animation: move-eyes-up-down 2s linear 1;
+  -webkit-animation: move-eyes-up-down 2s linear 1;
+  -moz-animation: move-eyes-up-down 2s linear 1;
+  -o-animation: move-eyes-up-down 2s linear 1;
+}
+
+.eyes-left-right {
+  animation: move-eyes-left-right 2s linear 1;
+  -webkit-animation: move-eyes-left-right 2s linear 1;
+  -moz-animation: move-eyes-left-right 2s linear 1;
+  -o-animation: move-eyes-left-right 2s linear 1;
+}
+
+.blink-eyes {
+  animation: blink-both-eyes 1s linear 1;
+  -webkit-animation: blink-both-eyes 1s linear 1;
+  -moz-animation: blink-both-eyes 1s linear 1;
+  -o-animation: blink-both-eyes 1s linear 1;
+}
+
+.line-1 {
+  animation: LightLines 2s linear 1;
+  -webkit-animation: LightLines 2s linear 1;
+  -moz-animation: LightLines 2s linear 1;
+  -o-animation: LightLines 2s linear 1;
+}
+
+.line-2 {
+  animation: LightLines 2s linear 1;
+  -webkit-animation: LightLines 2s linear 1;
+  -moz-animation: LightLines 2s linear 1;
+  -o-animation: LightLines 2s linear 1;
+  animation-delay: .15s;
+  -webkit-animation-delay: .15s;
+  -moz-animation-delay: .15s;
+  -o-animation-delay: .15s;
+ }
+
+.line-3 {
+  animation: LightLines 2s linear 1;
+  -webkit-animation: LightLines 2s linear 1;
+  -moz-animation: LightLines 2s linear 1;
+  -o-animation: LightLines 2s linear 1;
+  animation-delay: .3s;
+  -webkit-animation-delay: .3s;
+  -moz-animation-delay: .3s;
+  -o-animation-delay: .3s;
+}
+
+</style>
