89c3cae3545615c9b9126c793fa4128ae7f30517
Richard Porter <gifted.primate@protonmail.com>
2018-10-09 15:03:51 -0400

    removes all admin auth code that was using localcredential. admin ws secured by cookie containing player account

--- app/models/PlayerAccount.scala
+++ app/models/PlayerAccount.scala
@@ -1,5 +1,16 @@
 package models
 
+import play.api.libs.json.{Json, OFormat}
 import types.TokenId
 
-case class PlayerAccount(tokenId: TokenId, iss: String, sub: String, email: String, name: String, picture: Option[String] = None, isAdmin: Boolean = false)
+object PlayerAccount {
+  implicit val playerAccountFormat: OFormat[PlayerAccount] = Json.format[PlayerAccount]
+}
+
+case class PlayerAccount(tokenId: TokenId,
+                         iss: String,
+                         sub: String,
+                         email: String,
+                         name: String,
+                         picture: Option[String] = None,
+                         isAdmin: Boolean = false)
