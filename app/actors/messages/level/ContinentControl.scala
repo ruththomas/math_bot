888154ede204785c8a743c21607ab059d675c51e
Richard Porter <gifted.primate@protonmail.com>
2018-11-25 20:56:07 -0500

    adjusts coordinates grid locations

--- app/actors/messages/level/ContinentControl.scala
+++ app/actors/messages/level/ContinentControl.scala
@@ -49,7 +49,7 @@ object ContinentControl {
 
 class ContinentControl(parameters: List[String], description: String, functions: Functions) {
   import ContinentControl._
-  val listedFunctions = functions.list.values.toList
+  val listedFunctions: List[Function] = functions.list.values.toList
 
   def success(frame: Frame, problem: Problem): Boolean = {
     isFinalSpot(frame) && totalDropped(frame, problem) && checkParams(parameters, listedFunctions)
