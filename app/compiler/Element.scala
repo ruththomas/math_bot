08a95ec3ad26e87fdee9e7adade5054515fe4452
Richard Porter <gifted.primate@protonmail.com>
2018-12-17 11:22:19 -0500

    converts doubles to big decimal for summing lists of doubles, accounts for rounding of big decimals

--- app/compiler/Element.scala
+++ app/compiler/Element.scala
@@ -1,3 +1,5 @@
 package compiler
 
-case class Element(original: Boolean, name: String, image: String, color: ElementKinds.Value, value: Double)
+case class Element(original: Boolean, name: String, image: String, color: ElementKinds.Value, value: Double) {
+  val valueToBigDec: BigDecimal = BigDecimal(this.value)
+}
