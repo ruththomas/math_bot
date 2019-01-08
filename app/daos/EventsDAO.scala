ca282b83452f43e532ec8ac51cae7fdc9313bb65
joe <jchimien@gmail.com>
2019-01-08 00:06:16 -0500

    add index to 'id' field on event collection

--- app/daos/EventsDAO.scala
+++ app/daos/EventsDAO.scala
@@ -1,15 +1,18 @@
 package daos
 import actors.messages.admin.AdminEvent
 import com.google.inject.Inject
+import loggers.SemanticLog
 import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
 import org.bson.codecs.configuration.CodecRegistry
 import org.mongodb.scala.bson.codecs.{DEFAULT_CODEC_REGISTRY, Macros}
 import org.mongodb.scala.model.Filters.equal
+import org.mongodb.scala.model.{IndexOptions, Indexes}
 import org.mongodb.scala.{MongoCollection, MongoDatabase}
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
-class EventsDAO @Inject()(mathbotDb: MongoDatabase)(implicit ec: ExecutionContext) {
+class EventsDAO @Inject()(mathbotDb: MongoDatabase, aLogger: SemanticLog)(implicit ec: ExecutionContext) {
 
   import AdminEvent._
 
@@ -56,4 +59,16 @@ class EventsDAO @Inject()(mathbotDb: MongoDatabase)(implicit ec: ExecutionContex
   def getEvents: Future[Seq[AdminEvent]] = {
     collection.find().toFuture
   }
+
+  // creating index on id field as not using _id womp
+
+  collection
+    .createIndex(Indexes.ascending(dateLabel), IndexOptions().unique(true))
+    .toFuture()
+    .onComplete {
+      case Success(_) =>
+        aLogger.debug(SemanticLog.tags.index(collectionLabel, idLabel, "Created index"))
+      case Failure(t) =>
+        aLogger.error(SemanticLog.tags.index(collectionLabel, idLabel, t, "Failed to create index"))
+    }
 }
