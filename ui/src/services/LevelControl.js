00f634b716797e9720013f58af57492795dc28d6
Richard Porter <richard@mathbot.com>
2019-01-18 15:20:16 -0500

    ensures edit main is showing when positioning bar

--- ui/src/services/LevelControl.js
+++ ui/src/services/LevelControl.js
@@ -103,7 +103,8 @@ class LevelControl extends Ws {
   }
 
   _positionBar () {
-    if ($router.currentRoute.path === '/robot') {
+    const functionAreaShowing = $store.getters.getFunctionAreaShowing
+    if (functionAreaShowing === 'editMain' && $router.currentRoute.path === '/robot') {
       const $bar = $('.bar')
       const $mascot = $('.mascot')
       const $mainDropZone = $('.edit-main > .function-drop')
